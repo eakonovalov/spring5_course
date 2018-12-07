5531eeed40d83c7c01a60010377e1a78c984740e
Evgeny Konovalov <eakonovalov@gmail.com>
2018-12-07 13:03:45 +0300

    xml configuration

--- spring5-jokes/src/main/java/com/eakonovalov/spring5jokes/Spring5jokesApplication.java
+++ spring5-jokes/src/main/java/com/eakonovalov/spring5jokes/Spring5jokesApplication.java
@@ -2,7 +2,9 @@ package com.eakonovalov.spring5jokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource("classpath:jokes-configuration.xml")
 @SpringBootApplication
 public class Spring5jokesApplication {
 
