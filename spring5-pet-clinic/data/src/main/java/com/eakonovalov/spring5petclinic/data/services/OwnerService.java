9f4f3cdc5ddd2829d744c2fd0258368ddea2d0a4
Evgeny Konovalov <eakonovalov@gmail.com>
2018-12-07 18:24:56 +0300

    -

--- spring5-pet-clinic/data/src/main/java/com/eakonovalov/spring5petclinic/data/services/OwnerService.java
+++ spring5-pet-clinic/data/src/main/java/com/eakonovalov/spring5petclinic/data/services/OwnerService.java
@@ -1,7 +1,5 @@
 package com.eakonovalov.spring5petclinic.data.services;
 
-import com.eakonovalov.spring5petclinic.data.repositories.OwnerRepository;
-
 public interface OwnerService {
 
     void rename(Long id, String firsName);
