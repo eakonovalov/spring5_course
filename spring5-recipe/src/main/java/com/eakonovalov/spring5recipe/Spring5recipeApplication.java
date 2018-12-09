a703391fa20d8978a951072f0b2ac54b1c6ab857
Evgeny Konovalov <eakonovalov@gmail.com>
2018-12-09 23:33:14 +0300

    Create Recipe project

---
+++ spring5-recipe/src/main/java/com/eakonovalov/spring5recipe/Spring5recipeApplication.java
@@ -0,0 +1,12 @@
+package com.eakonovalov.spring5recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5recipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5recipeApplication.class, args);
+    }
+}
