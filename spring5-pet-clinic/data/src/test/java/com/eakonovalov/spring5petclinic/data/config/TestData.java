cad50e0cc45c7db118bf37378dc566661e2fb92d
Evgeny Konovalov <eakonovalov@gmail.com>
2018-12-10 17:21:35 +0300

    static resources

---
+++ spring5-pet-clinic/data/src/test/java/com/eakonovalov/spring5petclinic/data/config/TestData.java
@@ -0,0 +1,45 @@
+package com.eakonovalov.spring5petclinic.data.config;
+
+import com.eakonovalov.spring5petclinic.data.repositories.OwnerRepository;
+import com.eakonovalov.spring5petclinic.model.Owner;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestData {
+
+    private OwnerRepository ownerRepository;
+
+    public TestData(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @EventListener
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        Owner o = new Owner();
+        o.setFirstName("Ivan");
+        o.setLastName("Ivanov");
+        ownerRepository.save(o);
+
+        o = new Owner();
+        o.setFirstName("Petr");
+        o.setLastName("Petrov");
+        ownerRepository.save(o);
+
+        o = new Owner();
+        o.setFirstName("Alexander");
+        o.setLastName("Alexandrov");
+        ownerRepository.save(o);
+
+        o = new Owner();
+        o.setFirstName("Alexey");
+        o.setLastName("Alexeev");
+        ownerRepository.save(o);
+
+        o = new Owner();
+        o.setFirstName("Leonid");
+        o.setLastName("Leonidov");
+        ownerRepository.save(o);
+    }
+}
