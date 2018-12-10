88681de35c7de780109111ca21a7c9865f653075
Evgeny Konovalov <eakonovalov@gmail.com>
2018-12-10 17:36:33 +0300

    added layout to index page

---
+++ spring5-pet-clinic/web/src/main/java/com/eakonovalov/spring5petclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.eakonovalov.spring5petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "templates/index.html"})
+    public String index(){
+
+        return "index";
+    }
+}
